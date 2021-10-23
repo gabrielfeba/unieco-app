e3f8a56c3edd3af261fcde8bd93fc11f22c82d5a
gabriel.de.o.feba <gabriel.de.o.feba@accenture.com>
2021-10-23 16:41:50 -0300

    init commmit!

---
+++ src/test/java/com/unieco/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unieco.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
